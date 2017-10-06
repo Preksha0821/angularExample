06bde2fc9e9f322799efa620bfd201ca5ecadc8e
preksha0821 <preksha.21893@gmail.com>
2017-10-06 15:46:47 -0500

    Made story component

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId:module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class StoryComponent {
+story={id:100,name:'The Force Awakens'};
+color="blue";
 }
--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { StoryComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    StoryComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [StoryComponent]
 })
 export class AppModule { }
